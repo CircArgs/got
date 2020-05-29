41013134178d0d9f36903c9a5244d30f5eb15df4
CircArgs <quebecname@gmail.com>
2020-05-29 14:51:34 -0400

    switch to poetry

--- got/cli/actions.py
+++ got/cli/actions.py
@@ -12,8 +12,9 @@ class Init(argparse.Action):
         os.system("git init got_temp")
         with open(os.path.join("got_temp", ".git", ".gitignore"), "w") as gitignore:
             gitignore.write("*")
-        os.rename(os.path.join("got_temp", ".git"),
-                  os.path.join("got_temp", ".got"))
-        shutil.move(os.path.join("got_temp", ".got"),
-                    os.path.dirname(os.path.abspath("got_temp")))
+        os.rename(os.path.join("got_temp", ".git"), os.path.join("got_temp", ".got"))
+        shutil.move(
+            os.path.join("got_temp", ".got"),
+            os.path.dirname(os.path.abspath("got_temp")),
+        )
         shutil.rmtree("got_temp")
